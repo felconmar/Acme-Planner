9a02646da5f5ac97a9f786aa4a86dde716919a97
felconmar <felconmar@alum.us.es>
2021-04-24 02:00:36 +0200

    Task 15 #30:
    La funcionalidad de actualizar y borrar task de manager funciona, falta
    la parte de spam

---
+++ src/main/java/acme/features/manager/task/ManagerTaskUpdateService.java
@@ -0,0 +1,81 @@
+package acme.features.manager.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.roles.Manager;
+import acme.entities.tasks.Task;
+import acme.framework.components.Errors;
+import acme.framework.components.HttpMethod;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.components.Response;
+import acme.framework.helpers.PrincipalHelper;
+import acme.framework.services.AbstractUpdateService;
+
+@Service
+public class ManagerTaskUpdateService implements AbstractUpdateService<Manager, Task> {
+
+	
+	@Autowired
+	protected ManagerTaskRepository repository;
+	
+	@Override
+	public boolean authorise(final Request<Task> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public void bind(final Request<Task> request, final Task entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors);
+	}
+
+	@Override
+	public void unbind(final Request<Task> request, final Task entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+		
+		request.unbind(entity, model, "title", "startDate", "endDate", "workload", "description", "optionalLink", "visibility", "executionPeriod");
+	}
+
+	@Override
+	public Task findOne(final Request<Task> request) {
+		assert request != null;
+		return this.repository.findOneTaskFromId(request.getModel().getInteger("id"));
+	}
+
+	@Override
+	public void validate(final Request<Task> request, final Task entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		
+		
+	}
+
+	@Override
+	public void update(final Request<Task> request, final Task entity) {
+		assert request != null;
+		assert entity != null;
+
+		this.repository.save(entity);
+	}
+	
+	@Override
+	public void onSuccess(final Request<Task> request, final Response<Task> response) {
+		assert request != null;
+		assert response != null;
+
+		if (request.isMethod(HttpMethod.POST)) {
+			PrincipalHelper.handleUpdate();
+		}
+	}
+
+}
--- src/main/webapp/WEB-INF/views/master/menu.jsp
+++ src/main/webapp/WEB-INF/views/master/menu.jsp
@@ -37,11 +37,13 @@
 		<acme:menu-option code="master.menu.administrator" access="hasRole('Administrator')">
 			<acme:menu-suboption code="master.menu.administrator.user-accounts" action="/administrator/user-account/list"/>
 			<acme:menu-suboption code="master.menu.administrator.dashboard" action="/administrator/dashboard/show"/>
+			<acme:menu-suboption code="master.menu.administrator.spam" action="/administrator/spam/update" />
 			<acme:menu-separator/>
 			<acme:menu-suboption code="master.menu.administrator.populate-initial" action="/master/populate-initial"/>
 			<acme:menu-suboption code="master.menu.administrator.populate-sample" action="/master/populate-sample"/>			
 			<acme:menu-separator/>
 			<acme:menu-suboption code="master.menu.administrator.shutdown" action="/master/shutdown"/>
+			
 		</acme:menu-option>
 		
 		<acme:menu-option code="master.menu.authenticated"
@@ -54,6 +56,16 @@
 			access="hasRole('Provider')">
 			<acme:menu-suboption code="master.menu.provider.favourite-link"
 				action="http://www.example.com/" />
+		</acme:menu-option>
+		
+		<acme:menu-option code="master.menu.manager"
+			access="hasRole('Manager')">
+			<acme:menu-suboption code="master.menu.manager.favourite-link"
+				action="http://www.manager.com/" />
+			<acme:menu-suboption code="master.menu.manager.created-task"
+				action="/manager/task/create" />
+			<acme:menu-suboption code="master.menu.manager.listed-task"
+				action="/manager/task/list" />
 		</acme:menu-option>
 
 		<acme:menu-option code="master.menu.consumer"
@@ -83,6 +95,12 @@
 				access="!hasRole('Consumer')" />
 			<acme:menu-suboption code="master.menu.user-account.consumer"
 				action="/authenticated/consumer/update" access="hasRole('Consumer')" />
+				
+				
+				<acme:menu-suboption code="master.menu.user-account.become-manager"
+				action="/authenticated/manager/create"
+				access="!hasRole('Manager')" />
+				
 		</acme:menu-option>
 
 		<acme:menu-option code="master.menu.sign-out"
