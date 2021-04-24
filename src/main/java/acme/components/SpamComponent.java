59ec9eed7b90ed1ef540abf797ced3f8ecf05237
vicdiacor <vicdiacor@alum.us.es>
2021-04-24 17:22:27 +0200

    Task 010 completada

--- src/main/java/acme/components/SpamComponent.java
+++ src/main/java/acme/components/SpamComponent.java
@@ -3,16 +3,76 @@ package acme.components;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import acme.features.administrator.spam.AdministratorSpamRepository;
+import acme.entities.words.Word;
 
 
 public class SpamComponent {
 	
-	@Autowired
-	static AdministratorSpamRepository repository;
+	
+	
+	public static boolean containSpam(final String text, final List<Word> spamWords) {
+		final List<String> stringSpamWords= SpamComponent.listWordsToStrings(spamWords);
+		final List<List<String>> composedSpamWords = SpamComponent.getComposedSpamWords(stringSpamWords);
+		final List<String> wordsInput= SpamComponent.formatWordsInput(text);
+		System.out.println(stringSpamWords);
+		
+		final boolean res = false;
+		int spamCounter = 0;
+		int totalWordsCounter=0;
 
+		for ( int i=0; i<wordsInput.size(); ) { //Comprobamos si se trata de spam simple
+			final String w = wordsInput.get(i);
+			if(stringSpamWords.contains(w)) {
+				spamCounter++;
+				totalWordsCounter++;
+				i++;
+			}else{ //Comprobamos si se trata de una palabra de spam compuesta por varias palabras
+				
+				boolean coincide=false;
+				 int numIndexAdd=1;
+				for(int j=0; j<composedSpamWords.size(); j++) {
+					
+					final List<String>composedSpamWord = composedSpamWords.get(j);
+					if(!coincide && wordsInput.size()-i >= composedSpamWord.size()) {
+						 coincide = SpamComponent.containSpamRecursive(wordsInput,composedSpamWord,i,0);
+						 if(coincide) {
+							 numIndexAdd=composedSpamWord.size();
+							 break;
+						 	}
+						}
+					
+					}
+				if(coincide) {
+					 spamCounter++;
+					}
+				i+=numIndexAdd;
+				totalWordsCounter++;
+				}
+			}
+		
+		return SpamComponent.exceedsThreshold(10.0, spamCounter, totalWordsCounter);
+	
+		}
+	
+	public static boolean containSpamRecursive(final List<String>wordsInput,final List<String>composedSpamWord, final int i, final int c) {
+		
+		
+		final String inputWord= wordsInput.get(i);
+		final String composedWord= composedSpamWord.get(c);
+		
+	if(c<composedSpamWord.size()-1) {
+	
+		if(inputWord.equals(composedWord)) {
+			return SpamComponent.containSpamRecursive(wordsInput,composedSpamWord,i+1,c+1);
+		}else {
+			return false;
+		}
+		 
+	}else {
+		return inputWord.equals(composedWord);
+		}
+		
+	}
 	
 	public static List<List<String>> getComposedSpamWords(final List<String>spamWords){
 		final List<List<String>>res = new ArrayList<List<String>>();
@@ -47,48 +107,9 @@ public class SpamComponent {
 		return wordsInput;
 	}
 	
-	public static boolean containSpam(final String text, final List<String>spamWords) {
-		
-		final List<List<String>> composedSpamWords = SpamComponent.getComposedSpamWords(spamWords);
-		final List<String> wordsInput= SpamComponent.formatWordsInput(text);
-		
-		final boolean res = false;
-		int spamCounter = 0;
-		int totalWordsCounter=0;
-
-		for ( int i=0; i<wordsInput.size(); ) { //Comprobamos si se trata de spam simple
-			final String w = wordsInput.get(i);
-			if(spamWords.contains(w)) {
-				spamCounter++;
-				totalWordsCounter++;
-				i++;
-			}else{ //Comprobamos si se trata de una palabra de spam compuesta por varias palabras
-				
-				boolean coincide=false;
-				 int numIndexAdd=1;
-				for(int j=0; j<composedSpamWords.size(); j++) {
-					
-					final List<String>composedSpamWord = composedSpamWords.get(j);
-					if(!coincide && wordsInput.size()-i >= composedSpamWord.size()) {
-						 coincide = SpamComponent.containSpamRecursive(wordsInput,composedSpamWord,i,0);
-						 if(coincide) {
-							 numIndexAdd=composedSpamWord.size();
-							 break;
-						 	}
-						}
-					
-					}
-				if(coincide) {
-					 spamCounter++;
-					}
-				i+=numIndexAdd;
-				totalWordsCounter++;
-				}
-			}
-		
-		return SpamComponent.exceedsThreshold(10.0, spamCounter, totalWordsCounter);
 	
-		}
+	
+	
 	
 	public static final  boolean exceedsThreshold(final Double threshold,  final int spamCounter, final int totalWordsCounter) {
 
@@ -100,24 +121,16 @@ public class SpamComponent {
 		return res;
 		
 	}
-		
-	public static boolean containSpamRecursive(final List<String>wordsInput,final List<String>composedSpamWord, final int i, final int c) {
-		
-		
-		final String inputWord= wordsInput.get(i);
-		final String composedWord= composedSpamWord.get(c);
-		
-	if(c<composedSpamWord.size()-1) {
 	
-		if(inputWord.equals(composedWord)) {
-			return SpamComponent.containSpamRecursive(wordsInput,composedSpamWord,i+1,c+1);
-		}else {
-			return false;
+	public static List<String> listWordsToStrings(final List<Word> wordList){
+
+		final List<String> stringSpamWords= new ArrayList<String>();
+		for(final Word w:wordList) {
+			stringSpamWords.add(w.getWord());
 		}
-		 
-	}else {
-		return inputWord.equals(composedWord);
-		}
+		return stringSpamWords;
 		
 	}
+	
+	
 }
