da767c180c14ea9b434944e49f6e34ba56b787dc
migdurgon <migdurgon@alum.us.es>
2021-04-25 19:17:50 +0200

    Task-14 #29
    Modificacion metodo validate ManagerTaskCreateService

--- src/main/java/acme/features/manager/task/ManagerTaskCreateService.java
+++ src/main/java/acme/features/manager/task/ManagerTaskCreateService.java
@@ -93,6 +93,7 @@ public class ManagerTaskCreateService implements AbstractCreateService<Manager, 
 		assert !SpamComponent.containSpam(entity.getTitle(),spamWords, threshold);
 		assert !SpamComponent.containSpam(entity.getDescription(),spamWords, threshold);
 		assert !SpamComponent.containSpam(entity.getOptionalLink(),spamWords, threshold);
+		
 		}
 
 		@Override
