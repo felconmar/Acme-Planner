c256db4a39677c0292147673ca595ccc05f7d2f7
vicdiacor <vicdiacor@alum.us.es>
2021-05-03 12:13:21 +0200

    Task 003: Bug arreglado, mostrar solo los shouts recientes

--- src/main/java/acme/entities/tasks/Task.java
+++ src/main/java/acme/entities/tasks/Task.java
@@ -3,7 +3,7 @@ package acme.entities.tasks;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Digits;
@@ -57,7 +57,7 @@ public class Task extends DomainEntity{
 	protected Boolean finished;
 	
 	@NotNull
-	@OneToOne(optional=false)
+	@ManyToOne(optional=false)
 	protected Manager manager;
 	
 	@Min(0)
